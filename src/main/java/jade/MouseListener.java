1d0bced6a1f676d74d218828ff064759fdd2e8e0
Sarah <84047032+saraLEGEND@users.noreply.github.com>
2022-11-11 20:42:32 -0500

    Create Window instance

---
+++ src/main/java/jade/MouseListener.java
@@ -0,0 +1,33 @@
+package jade;
+
+public class MouseListener {
+    private static MouseListener instance;
+    private double scrollX, scrollY;
+    private double xPos, yPos, lastY, lastX;
+    private boolean mouseButtonPressed[] = new boolean[3];
+    private boolean isDragging;
+
+    private MouseListener() {
+        this.scrollX = 0.0;
+        this.scrollY = 0.0;
+        this.xPos = 0.0;
+        this.yPos = 0.0;
+        this.lastX = 0.0;
+        this.lastY = 0.0;
+        this.isDragging = false;
+    }
+
+    public static MouseListener get() {
+        if (MouseListener.instance == null) {
+            MouseListener.instance = new MouseListener();
+        }
+        return MouseListener.instance;
+    }
+
+    public static void mousePosCallback(long window, double xPos, double yPos) {
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
+        get().xPos = xPos;
+        get().yPos = yPos;
+    }
+}
